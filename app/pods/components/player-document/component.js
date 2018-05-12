fd6d8e02553e297eb7405622b645c17e784129b1
aayusharora <aayush113002@gmail.com>
2018-05-12 19:54:04 +0530

    Fix pdf

--- app/pods/components/player-document/component.js
+++ app/pods/components/player-document/component.js
@@ -2,5 +2,6 @@ import Component from '@ember/component';
 import { alias } from 'ember-decorators/object/computed';
 
 export default class DocumentComponent extends Component {
+  classNames = ['height-100'];
   @alias("payload") document
 }
